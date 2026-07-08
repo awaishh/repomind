b79a507f4a947e2abc15b445b0fc4f70bf0c07e5
Awaish Ehsan <awaishehsan077@gmail.com>
2026-07-08 17:01:15 +0530

    add auth and validator middleware

---
+++ backend/src/middlewares/auth.middleware.js
@@ -0,0 +1,31 @@
+import User from '../models/user.models.js';
+import { ApiError } from '../utils/api-error.js';
+import { asyncHandler } from '../utils/async-handler.js';
+import jwt from 'jsonwebtoken';
+
+export const verifyJWT = asyncHandler(async (req, res, next) => {
+    const token =
+        req.cookies?.accessToken ||
+        req.header("Authorization")?.replace("Bearer ", "");
+
+    if (!token) {
+        throw new ApiError(401, "Unauthorized Request");
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+        const user = await User.findById(decoded?._id).select(
+            "-password -refreshToken"
+        );
+
+        if (!user) {
+            throw new ApiError(401, "Invalid Access Token");
+        }
+
+        req.user = user;
+        next();
+    } catch (err) {
+        throw new ApiError(401, "Invalid or expired token");
+    }
+});
